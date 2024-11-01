2ba68577f41ca96817f5b2fa4ee32b59852198d9
Erik Veld <erik.veld@gmail.com>
2024-11-01 08:33:17 +0100

    change colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import rehypePrettyCode from "rehype-pretty-code";
 
 import sitemap from '@astrojs/sitemap';
 
@@ -7,4 +8,9 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://erikveld.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		shikiConfig: {
+			theme: 'catppuccin-latte',
+		},
+  },
 });
